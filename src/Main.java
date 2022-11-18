f2849a77ff7e6b960d6c759371cceddddb5e11d7
Sergistan <87235850+Sergistan@users.noreply.github.com>
2022-11-18 12:24:01 +0300

    Update comment

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1,1);
-        int c = calc.devide.apply(a, b); // ArithmeticException, т.к. на переменая b=0, а на ноль делить нельзя!
+        int c = calc.devide.apply(a, b); // ArithmeticException, т.к. переменая b=0, а на ноль делить нельзя!
         boolean x = calc.isPositive.test(a);
         int z = calc.pow.apply(a);
 
